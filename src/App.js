84f8457affdb0398e7a29b1f72ba7733cc52e24b
Zaidah Yunos <152250153+zaidahyunos@users.noreply.github.com>
2024-06-12 13:41:27 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Weather from "./Weather";
+
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container mt-3 px-5">
+        <footer>
+          <p className="fs-6">
+            This project was coded by {""}
+            <a
+              href="http://github.com/zaidahyunos"
+              target="_blank"
+              className="link"
+            >
+              Zaidah Yunos,
+            </a>{" "}
+            is open-sourced on {""}
+            <a href="http://"></a>
+          </p>
+        </footer>
+      </div>
+    </div>
+  );
+}
